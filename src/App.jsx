737852a38576e3587d0fa820d56d033e853b7c80
Ievgen Dilevskyi <e.dilevskiy@gmail.com>
2024-04-29 12:14:12 -0600

    Not all side effects need useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,15 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    // This side effect doesn't need useEffect because it runs only on function call
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
